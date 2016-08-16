7a68b02ce771fca6ee59af8d9592dcd9ef633120
Sergey Zubtsovskiy <sergey.zubtsovskiy@gmail.com>
2016-08-16 20:38:45 +0200

    Use elm-webpack-starter's artifacts

---
+++ src/index.js
@@ -0,0 +1,9 @@
+require('./styles/main.scss');
+global.jQuery = require('jquery');
+require('bootstrap');
+
+// inject bundled Elm app into div#main
+const Direct = require('./Direct').Direct;
+const Reversed = require('./Reversed').Reversed;
+Direct.embed(document.getElementById('container-direct'));
+Reversed.embed(document.getElementById('container-reversed'));
