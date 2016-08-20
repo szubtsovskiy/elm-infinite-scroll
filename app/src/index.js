26e53eb3ebab3a5e3eb8cbc863dccbcd00f3bd79
Sergey Zubtsovskiy <sergey.zubtsovskiy@gmail.com>
2016-08-20 17:32:25 +0200

    Use elm-lang/dom package to scroll down in reversed version

--- app/src/index.js
+++ app/src/index.js
@@ -7,13 +7,5 @@ const Direct = require('./features/Direct').Direct;
 const Reversed = require('./features/Reversed').Reversed;
 Direct.embed(document.getElementById('container-direct'), require('./features/Direct.scss'));
 global.jQuery('a[href="#reversed"]').on('shown.bs.tab', function () {
-  let app = Reversed.embed(document.getElementById('container-reversed'), require('./features/Reversed.scss'));
-  app.ports.scroll.subscribe(function(cmd) {
-    let containerId = cmd[0];
-    let scrollTop = cmd[1];
-    setTimeout(function() {
-      var container = document.getElementById(containerId);
-      container.scrollTop = scrollTop;
-    }, 50);
-  })
+  Reversed.embed(document.getElementById('container-reversed'), require('./features/Reversed.scss'));
 });
